b71185885b38e352865fc8151774ed6ad754b522
Ragan Hagey <raganahagey@gmail.com>
2022-09-08 15:03:14 -0400

    commit tree c54fa3391da4f21660267e2734571d710a3be9a1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+ const element = document.getElementById("main");  
+
+ element.remove();
+
+
+ let newHeader = document.createElement("h1");
+  newHeader.setAttribute("id","victory");
+
+ 
+ newHeader.innerHTML = "Sunny is the champion";
